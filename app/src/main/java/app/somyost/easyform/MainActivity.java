f1c08679d6084920e2c07d3280a6dd4ec0de540f
masterUNG <phrombutr@gmail.com>
2017-09-17 11:00:23 +0700

    Design UI Main OK V2

--- app/src/main/java/app/somyost/easyform/MainActivity.java
+++ app/src/main/java/app/somyost/easyform/MainActivity.java
@@ -3,11 +3,20 @@ package app.somyost.easyform;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.somyost.easyform.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment())
+                    .commit();
+        }
+
     }
 }
